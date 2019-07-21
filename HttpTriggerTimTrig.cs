a7509552a153e0f78a2dee7d0693611e4eb21a37
jsonk952 <jsonk952@gmail.com>
2019-07-21 21:45:40 +1200

    added test file read

--- HttpTriggerTimTrig.cs
+++ HttpTriggerTimTrig.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -24,9 +25,16 @@ namespace Company.Function
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
-
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create("https://timfuncappbd87.file.core.windows.net/test1/azure_test_1.txt?sp=rl&st=2019-07-21T09:01:17Z&se=2019-07-22T09:01:17Z&sv=2018-03-28&sig=OphHu9ghDvJ9sSyiOmqqH1RFWGOXJpV%2Fs0D9WvQTNCE%3D&sr=f");
+            WebResponse resp = myReq.GetResponse();
+            string body = "unk";
+            using (var reader = new StreamReader(resp.GetResponseStream()))
+            {
+                body = reader.ReadToEnd(); // do something fun...
+            }
+            
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello dude, {name}")
+                ? (ActionResult)new OkObjectResult($"Hello dude, {name} {body}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
